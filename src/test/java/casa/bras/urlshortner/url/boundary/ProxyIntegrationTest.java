27270e366121d7fe63712711d6f271f4d32fd323
nuno.bras <nuno.bras@hotmail.com>
2024-04-20 18:24:20 +0100

    adds test to ProxyResource

---
+++ src/test/java/casa/bras/urlshortner/url/boundary/ProxyIntegrationTest.java
@@ -0,0 +1,56 @@
+package casa.bras.urlshortner.url.boundary;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.Response.Status.CREATED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import casa.bras.urlshortner.url.dto.CreateUrlDTO;
+import casa.bras.urlshortner.url.dto.UrlProxyDTO;
+import casa.bras.urlshortner.url.entity.UrlRepository;
+import casa.bras.urlshortner.users.entity.UserEntity;
+import casa.bras.urlshortner.users.entity.UserRepository;
+import casa.bras.utilities.IntegrationTest;
+import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ProxyIntegrationTest extends IntegrationTest {
+
+  @Inject UrlRepository urlRepository;
+  @Inject UserRepository userRepository;
+
+  protected UserEntity user;
+
+  @BeforeEach
+  void beforeEach() {
+
+    userRepository.deleteAll();
+    urlRepository.deleteAll();
+
+    user = new UserEntity("test@test.test", "name");
+    userRepository.persist(user);
+  }
+
+  protected UrlProxyDTO createAndAssertProxy(CreateUrlDTO request) {
+    var response =
+        given()
+            .contentType(ContentType.JSON)
+            .body(request)
+            .header(UrlProxyResource.API_KEY_HEADER, user.getApiKey())
+            .when()
+            .post("/urls")
+            .then()
+            .statusCode(CREATED.getStatusCode())
+            .extract()
+            .as(UrlProxyDTO.class);
+
+    assertNotNull(response);
+    assertEquals(request.url(), response.url());
+    assertNotNull(response.hash());
+
+    assertNotNull(urlRepository.findByHash(response.hash()));
+
+    return response;
+  }
+}
